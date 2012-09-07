1625d085c0b817ff00e352c787dea877119c1e64
Stephen Paul Weber <singpolyma@singpolyma.net>
2012-09-07 08:34:24 -0500

    cleanup example code

--- example/Main.hs
+++ example/Main.hs
@@ -5,22 +5,23 @@ import Data.String (fromString)
 import qualified Data.Vault as Vault
 
 import Network.Wai
-import Network.Wai.Session (withSession)
+import Network.Wai.Session (withSession, Session)
 import Network.Wai.Session.Map (mapStore_)
 import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Types (ok200)
+import Control.Monad.Trans.Resource (ResourceT)
+
+app :: Vault.Key (Session (ResourceT IO) String String) -> Application
+app session env = do
+	u <- sessionLookup "u"
+	sessionInsert "u" insertThis
+	return $ responseLBS ok200 [] $ maybe (fromString "Nothing") fromString u
+	where
+	insertThis = show $ pathInfo env
+	Just (sessionLookup, sessionInsert) = Vault.lookup session (vault env)
 
 main :: IO ()
 main = do
-	vk <- Vault.newKey
+	session <- Vault.newKey
 	store <- mapStore_
-	run 3000 (withSession store (fromString "SESSION") def vk (\req -> do
-			let Just (sessionLookup, sessionInsert) = Vault.lookup vk (vault req)
-			u <- sessionLookup "u"
-			sessionInsert "u" (show $ pathInfo req)
-			case u of
-				Just au ->
-					return $ responseLBS ok200 [] (fromString au)
-				Nothing ->
-					return $ responseLBS ok200 [] (fromString "Nothing")
-		))
+	run 3000 $ withSession store (fromString "SESSION") def session $ app session
